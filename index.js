519b210aef0a4f00112743232d48be2bc374488d
Arthur Ragimov <arthur.ragimov@gmail.com>
2019-04-14 19:16:01 +0300

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const Telegraf = require('telegraf');
 
 const APP_URL = process.env.APP_URL || 'http://localhost:5000';
-const TELEGRAM_WEBHOOK_URI = process.env.TELEGRAM_WEBHOOK_URI || '/telegram-bot';
+const TELEGRAM_WEBHOOK_URI = process.env.TELEGRAM_WEBHOOK_URI || '/telegram-bot-alt';
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 
 const bot = new Telegraf(BOT_TOKEN);
@@ -15,14 +15,18 @@ const bot = new Telegraf(BOT_TOKEN);
 router.get('/', (req, res) => res.sendFile(path.join(__dirname + '/views/pages/index.html')));
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bot.webhookCallback(APP_URL + TELEGRAM_WEBHOOK_URI));
+// app.use(bot.webhookCallback(APP_URL + TELEGRAM_WEBHOOK_URI));
 app.use('/', router);
 
-bot.telegram.setWebhook(APP_URL + TELEGRAM_WEBHOOK_URI);
+// bot.telegram.setWebhook(APP_URL + TELEGRAM_WEBHOOK_URI);
+//
+// bot.on('text', (ctx) => {
+//     ctx.reply('hello world');
+// });
 
-bot.on('text', (ctx) => {
-    ctx.reply('hello world');
-});
+bot.start((ctx) => ctx.reply('hello world'));
+bot.on('text', (ctx) => ctx.reply(`you say: ${ctx.message}`))
+bot.launch();
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
